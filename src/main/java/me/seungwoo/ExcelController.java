e9ed4894561e60a455a3001491186f860e03f76e
sonseungwoo <skyghy3342@naver.com>
2019-04-30 17:03:23 +0900

    commit

--- src/main/java/me/seungwoo/ExcelController.java
+++ src/main/java/me/seungwoo/ExcelController.java
@@ -43,44 +43,37 @@ public class ExcelController {
 
     @GetMapping("/excelDown")
     public void excelDown(HttpServletResponse response) throws Exception {
-        // 게시판 목록조회
+        // DB조회
         List<Person> list = Arrays.asList(
                 new Person("사람1", 10, "test@test.com"),
                 new Person("사람2", 20, "test@test.com"),
                 new Person("사람3", 30, "test@test.com")
         );
-
-        // 워크북 생성
         Workbook wb = new HSSFWorkbook();
-        Sheet sheet = wb.createSheet("게시판");
+        Sheet sheet = wb.createSheet("테스트");
         Row row;
         Cell cell;
         int rowNo = 0;
 
-        // 테이블 헤더용 스타일
+        // 테이블 헤더 스타일
         CellStyle headStyle = wb.createCellStyle();
-
-        // 가는 경계선을 가집니다.
         headStyle.setBorderTop(BorderStyle.THIN);
         headStyle.setBorderBottom(BorderStyle.THIN);
         headStyle.setBorderLeft(BorderStyle.THIN);
         headStyle.setBorderRight(BorderStyle.THIN);
-
-        // 배경색은 노란색입니다.
+        // 헤더 배경색 설정
         headStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.YELLOW.getIndex());
         headStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-        // 데이터는 가운데 정렬합니다.
         headStyle.setAlignment(HorizontalAlignment.CENTER);
 
-        // 데이터용 경계 스타일 테두리만 지정
+        //테두리 지정
         CellStyle bodyStyle = wb.createCellStyle();
         bodyStyle.setBorderTop(BorderStyle.THIN);
         bodyStyle.setBorderBottom(BorderStyle.THIN);
         bodyStyle.setBorderLeft(BorderStyle.THIN);
         bodyStyle.setBorderRight(BorderStyle.THIN);
 
-        // 헤더 생성
+        // 헤더 추가 생성
         row = sheet.createRow(rowNo++);
         cell = row.createCell(0);
         cell.setCellStyle(headStyle);
@@ -92,7 +85,7 @@ public class ExcelController {
         cell.setCellStyle(headStyle);
         cell.setCellValue("이메일");
 
-        // 데이터 부분 생성
+        // 데이터 부분 생성 추가로 생성
         for (Person person : list) {
             row = sheet.createRow(rowNo++);
             cell = row.createCell(0);
@@ -106,11 +99,8 @@ public class ExcelController {
             cell.setCellValue(person.getEmail());
         }
 
-        // 컨텐츠 타입과 파일명 지정
         response.setContentType("ms-vnd/excel");
         response.setHeader("Content-Disposition", "attachment;filename=person.xls");
-
-        // 엑셀 출력
         wb.write(response.getOutputStream());
         wb.close();
     }
